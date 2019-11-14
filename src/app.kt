5588b73637823158d52c89320bcb854e4e1fec79
Vera Alexeeva <vera.cortie@gmail.com>
2019-11-14 16:48:42 +0300

    I have changed this ...

--- src/app.kt
+++ src/app.kt
@@ -1,3 +1,10 @@
+/*
+OK, this is a simple app
+*/
+
 fun main() {
-    println("This is kotlin from idea")
+    println("This is kotlin from idea, really")
+    println("This is it")
 }
+
+// the end
